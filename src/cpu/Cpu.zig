998178d8c441c4b7158a34c39f19774b99d47651
Wyatt Radkiewicz <wyattwradkiewicz@gmail.com>
2025-06-01 18:25:37 -0500

    Got rid of unneeded inline

--- src/cpu/Cpu.zig
+++ src/cpu/Cpu.zig
@@ -1,3 +1,5 @@
+const std = @import("std");
+
 const Isa = @import("Isa.zig");
 pub const View = @import("View.zig");
 
@@ -25,20 +27,52 @@ const Stub = struct {
     }
 };
 
-const Fetch = struct {
-    pub inline fn op(comptime stage: u2, view: *View) void {
-        _ = view;
-        switch (stage) {
-            0 => {},
-            1, 2 => {},
-            3 => {},
+fn Read(comptime to: []const u8, assert_addr: fn (*View) u16) type {
+    return struct {
+        pub fn op(comptime stage: u2, view: *View) void {
+            switch (stage) {
+                0 => {
+                    view.*.pins.addr = assert_addr(view);
+                    view.*.pins.rd = true;
+                    view.*.pins.wr = false;
+                    view.*.pins.cs = true;
+                },
+                1 => {
+                    view.*.pins.cs = switch (view.*.pins.addr) {
+                        0xA000...0xFDFF => false,
+                        else => true,
+                    };
+                },
+                2 => {},
+                3 => {
+                    @field(view.*.regs, to) = view.*.pins.data;
+                },
+            }
         }
+    };
+}
+
+const Fetch = struct {
+    pub fn assertAddr(view: *View) u16 {
+        view.*.regs.pc.word += 1;
+        return view.*.regs.pc.word;
+    }
+
+    pub fn op(comptime stage: u2, view: *View) void {
+        Read("ir", assertAddr).op(stage, view);
     }
 };
 
 test "nop" {
     var cpu = reset;
     cpu.tick();
+    try std.testing.expectEqual(View.Pins{
+        .addr = 1,
+        .data = 0,
+        .cs = true,
+        .rd = true,
+        .wr = false,
+    }, cpu.view.pins);
     cpu.tick();
     cpu.tick();
     cpu.tick();
